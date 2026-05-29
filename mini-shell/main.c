1d3a35904c23e0715f37e70d581b09eb50a42134
idk-xd-lol <ya.xd.work.02@gmail.com>
2026-05-29 20:40:25 +0200

    mini shell

---
+++ mini-shell/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "functions.h"
+
+int main()
+{
+  char *buffer;
+  while (1)
+  {
+  }
+  return 0;
+}
+
+int getcommand(const char *buffer)
+{
+  int c;
+  while((c = getchar()) != '\n' && c != EOF)
+  {
+
+  }
+  return 0;
+}
+
